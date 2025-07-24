d2f7e62c482da440b3b12945ccf3def12e6e32b4
Chandrashekar_Reddy <chandrashekar@aiyensi.com>
2025-07-24 10:55:19 +0530

    update code and add test scripts

---
+++ jewelleryApi/TestScript/testConfig.py
@@ -0,0 +1,8 @@
+loginUrl = "http://localhost:8000/login"
+adminEmail = "admin@example.com"
+adminPassword = "asdfgh"
+
+CATEGORY_URL = "http://localhost:8000/admin/categories"
+PRODUCT_URL = "http://localhost:8000/admin/product/create"
+UPLOAD_URL = "http://localhost:8000/auth/upload-file"
+
